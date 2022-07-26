d60b134315078a406cf89a26e7488a36ee26f0d2
Belen Neme <belenneme1@gmail.com>
2022-07-26 08:52:09 -0300

    cambio de puerto

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 
 //=================Puerto====================
-process.env.PORT= process.env.PORT || 3000
+process.env.PORT= process.env.PORT || 3008
 
 //--------Definir entornos------------
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
